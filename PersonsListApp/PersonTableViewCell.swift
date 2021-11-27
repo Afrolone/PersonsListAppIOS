77d1557350facb2142c7e852ddea6374511225fd
Jahja Muratovic <muratow_nur@hotmail.com>
2021-11-27 23:06:20 +0100

    Finalised the task.

---
+++ PersonsListApp/PersonTableViewCell.swift
@@ -0,0 +1,75 @@
+//
+//  PersonTableViewCell.swift
+//  PersonsListApp
+//
+//  Created by Ant Colony on 27. 11. 2021..
+//
+
+import UIKit
+import Kingfisher
+
+class PersonTableViewCell: UITableViewCell {
+    
+    var person: Person?
+    var navigationController: UINavigationController?
+
+
+    @IBOutlet weak var mainCellView: UIView!
+    @IBOutlet weak var avatar: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var surnameLabel: UILabel!
+    @IBOutlet weak var ageLabel: UILabel!
+    @IBOutlet weak var genderLabel: UILabel!
+    @IBOutlet weak var numberLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        mainCellView.layer.borderColor = UIColor.black.cgColor
+        mainCellView.layer.borderWidth = 1.0
+        mainCellView.layer.cornerRadius = 10
+        
+        let singleTap = UITapGestureRecognizer(target: self, action: #selector(tapDetected))
+        avatar.isUserInteractionEnabled = true
+        avatar.addGestureRecognizer(singleTap)
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    @objc func tapDetected() {
+        print("Imageview Clicked")
+//        let movie = MoviesCollection.movies[indexPath.row]
+//        let vc = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: Constants.Storyboard.movieDetailViewController) as! MovieDetailViewController
+//        vc.movie = movie
+//        navigationController?.pushViewController(vc, animated: true)
+        let vc = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "DetailViewController") as! DetailViewController
+        vc.person = person
+        navigationController?.pushViewController(vc, animated: true)
+    }
+    
+    
+    func setTheCell(_person: Person, navCont: UINavigationController?) {
+        person = _person
+        navigationController = navCont
+        nameLabel.text = person?.firstName
+        surnameLabel.text = person?.lastName
+        if let age = person?.age {
+            ageLabel.text = "Age: \(age)"
+        }
+        genderLabel.text = person?.gender
+        numberLabel.text = person?.number
+        
+    
+        avatar.kf.setImage(with: person?.avatar)
+        avatar.layer.masksToBounds = false
+        avatar.layer.cornerRadius = avatar.frame.width/2
+        avatar.layer.borderWidth = 1
+        avatar.layer.borderColor = UIColor.clear.cgColor
+        avatar.clipsToBounds = true
+    }
+
+}
--- PersonsListApp/ViewController.swift
+++ PersonsListApp/ViewController.swift
@@ -7,19 +7,49 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    var data = [Person]()
+    
+    @IBOutlet var tableView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        let data = DataLoader().personsData
+        data = DataLoader().personsData
+        
+        
+        let nib = UINib(nibName: "PersonTableViewCell", bundle: nil)
+        tableView.register(nib, forCellReuseIdentifier: "PersonTableViewCell")
+        tableView.delegate = self
+        tableView.dataSource = self
         
         print("something!")
         for person in data {
             print(person.firstName)
         }
     }
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        data.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "PersonTableViewCell", for: indexPath) as? PersonTableViewCell
+        else { fatalError("Could not create PersonCell") }
+        let person = data[indexPath.row]
+        cell.setTheCell(_person: person, navCont: navigationController)
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let person = data[indexPath.row]
+        
+//        let vc = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: Constants.Storyboard.movieDetailViewController) as! MovieDetailViewController
+//        vc.movie = movie
+//        navigationController?.pushViewController(vc, animated: true)
+    }
 
 }
 
